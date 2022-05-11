e1b18554b24a4bce4fee096092bc22829df52c42
Hemanth-046 <96861160+Hemanth-046@users.noreply.github.com>
2022-05-11 10:27:58 +0530

    Add files via upload

---
+++ Pandas.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+pip install pandas
+
+
+# In[5]:
+
+
+import pandas as a
+c = {
+  'order': ["Veg", "Non-Veg", "Bevrage"],
+  'price': [500, 700, 200]
+}
+b = a.DataFrame(c)
+print(b)
+
+
+# In[11]:
+
+
+import pandas as a
+b = a.read_csv('E:\\Data.CSV')
+print(b) 
+
+
+# In[17]:
+
+
+import pandas as a
+b = [1,4,3]
+c = a.Series(b)
+print(c)
+
+
+# In[21]:
+
+
+import pandas as a
+c = {
+  'Name': ["Venky", "Suhrut", "Murli", "Hemanth"],
+  'Age': [50, 45, 40, 17],
+  'Gender': ["Male", "Male", "Male", "Male"],
+}
+b = a.DataFrame(c)
+print(b)
+
+
+# In[ ]:
+
+
+
+
